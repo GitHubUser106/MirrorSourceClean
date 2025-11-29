c18c644e2a46b1be70cb705a025f8dfdd5651f15
John Tomchick <johntomchick@gmail.com>
2025-11-29 08:37:37 -0800

    feat: add visible install banner for mobile

--- src/components/InstallPromptWrapper.tsx
+++ src/components/InstallPromptWrapper.tsx
@@ -2,10 +2,10 @@
 
 import dynamic from "next/dynamic";
 
-const InstallPrompt = dynamic(() => import("./InstallPrompt"), {
+const InstallBanner = dynamic(() => import("./InstallBanner"), {
   ssr: false,
 });
 
 export default function InstallPromptWrapper() {
-  return <InstallPrompt />;
+  return <InstallBanner />;
 }
