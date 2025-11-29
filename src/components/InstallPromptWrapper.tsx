0965f2607682e511fdc68ca7f6b42fb3b98d0f45
John Tomchick <johntomchick@gmail.com>
2025-11-29 12:21:55 -0800

    fix: remove dynamic import from wrapper

--- src/components/InstallPromptWrapper.tsx
+++ src/components/InstallPromptWrapper.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const InstallBanner = dynamic(() => import("./InstallBanner"), {
-  ssr: false,
-});
+import InstallBanner from "./InstallBanner";
 
 export default function InstallPromptWrapper() {
   return <InstallBanner />;
