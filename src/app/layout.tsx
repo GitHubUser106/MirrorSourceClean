37807dde85579e9d7d2de4b8cd2835c104afc6e6
John Tomchick <johntomchick@gmail.com>
2025-12-11 08:57:40 -0800

    Add Google Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="Obu9ZJp1ML2sUHm2P5LfOGNNX4FjlX9p9wDI-s_N3Qo" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
